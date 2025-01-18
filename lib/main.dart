fb2cd12e490e6782027841c77c6cdd40a6bd16a2
ponyatado <galeanoalejandro@gmail.com>
2025-01-18 17:54:17 +0100

    v0.09

--- lib/main.dart
+++ lib/main.dart
@@ -62,6 +62,46 @@ class Environment {
     }
     return dotenv.env['VAR_06'] ?? 'VAR_06_FALLBACK';
   }
+
+  static String get varPruebaLarga01 {
+    const systemEnv = String.fromEnvironment('VAR_PRUEBA_LARGA_01');
+    if (systemEnv.isNotEmpty) {
+      return systemEnv;
+    }
+    return dotenv.env['VAR_PRUEBA_LARGA_01'] ?? 'VAR_PRUEBA_LARGA_01_FALLBACK';
+  }
+
+  static String get varPruebaLarga02 {
+    const systemEnv = String.fromEnvironment('VAR_PRUEBA_LARGA_02');
+    if (systemEnv.isNotEmpty) {
+      return systemEnv;
+    }
+    return dotenv.env['VAR_PRUEBA_LARGA_02'] ?? 'VAR_PRUEBA_LARGA_02_FALLBACK';
+  }
+
+  static String get varPruebaLarga03 {
+    const systemEnv = String.fromEnvironment('VAR_PRUEBA_LARGA_03');
+    if (systemEnv.isNotEmpty) {
+      return systemEnv;
+    }
+    return dotenv.env['VAR_PRUEBA_LARGA_03'] ?? 'VAR_PRUEBA_LARGA_03_FALLBACK';
+  }
+
+  static String get varPruebaLarga04 {
+    const systemEnv = String.fromEnvironment('VAR_PRUEBA_LARGA_04');
+    if (systemEnv.isNotEmpty) {
+      return systemEnv;
+    }
+    return dotenv.env['VAR_PRUEBA_LARGA_04'] ?? 'VAR_PRUEBA_LARGA_04_FALLBACK';
+  }
+
+  static String get varPruebaLarga05 {
+    const systemEnv = String.fromEnvironment('VAR_PRUEBA_LARGA_05');
+    if (systemEnv.isNotEmpty) {
+      return systemEnv;
+    }
+    return dotenv.env['VAR_PRUEBA_LARGA_05'] ?? 'VAR_PRUEBA_LARGA_05_FALLBACK';
+  }
 }
 
 Future<void> main() async {
@@ -110,6 +150,16 @@ class EnvironmentVariablePage extends StatelessWidget {
             Text('VAR 05: ${Environment.var05}'),
             const SizedBox(height: 20),
             Text('VAR 06: ${Environment.var06}'),
+            const SizedBox(height: 20),
+            Text('PRUEBA LARGA 01: ${Environment.varPruebaLarga01}'),
+            const SizedBox(height: 20),
+            Text('PRUEBA LARGA 02: ${Environment.varPruebaLarga02}'),
+            const SizedBox(height: 20),
+            Text('PRUEBA LARGA 03: ${Environment.varPruebaLarga03}'),
+            const SizedBox(height: 20),
+            Text('PRUEBA LARGA 04: ${Environment.varPruebaLarga04}'),
+            const SizedBox(height: 20),
+            Text('PRUEBA LARGA 05: ${Environment.varPruebaLarga05}'),
           ],
         ),
       ),
